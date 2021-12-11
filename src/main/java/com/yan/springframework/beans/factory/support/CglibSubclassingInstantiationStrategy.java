ecffbdfadbfdb8c6f74bb741850bc46e37b3085d
yanyingjie <yan_yj1997@163.com>
2021-12-11 16:51:05 +0800

    yan-yj,fix:上次遗漏了几个文件

---
+++ src/main/java/com/yan/springframework/beans/factory/support/CglibSubclassingInstantiationStrategy.java
@@ -0,0 +1,33 @@
+package com.yan.springframework.beans.factory.support;
+
+import com.yan.springframework.beans.BeansExcepton;
+import com.yan.springframework.beans.factory.config.BeanDefinition;
+import net.sf.cglib.proxy.Enhancer;
+import net.sf.cglib.proxy.NoOp;
+
+import java.lang.reflect.Constructor;
+
+/**
+ * CglibSubclassingInstantiationStrategy
+ *
+ * @description:
+ * @author: yan-yj
+ * @time: 2021/12/11 15:16
+ */
+public class CglibSubclassingInstantiationStrategy implements InstantiationStrategy{
+    @Override
+    public Object instantiate(BeanDefinition beanDefinition, String beanName, Constructor ctor, Object[] args) throws BeansExcepton {
+        Enhancer enhancer = new Enhancer();
+        enhancer.setSuperclass(beanDefinition.getBeanclass());
+        enhancer.setCallback(new NoOp() {
+            @Override
+            public int hashCode() {
+                return super.hashCode();
+            }
+        });
+       if (ctor == null){
+           return enhancer.create();
+       }
+       return enhancer.create(ctor.getParameterTypes(), args);
+    }
+}
